3aa28f9ba97c45599bd202d78e7c1975ea7cb72b
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-24 18:30:47 +0300

    commit tree 18ca94799bfb61ad9e45d9f68f1c1a7be8749914

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 const init = () => {
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // console.log(event);
+        const input = document.querySelector('input#searchByID');
+
+  console.log(input.value);
+
+  fetch('http://localhost:3000/movies')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // LOG: (3) [{…}, {…}, {…}]
+  });
+
+    });
   
 }
 
